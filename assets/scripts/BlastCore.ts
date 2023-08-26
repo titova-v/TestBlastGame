f9d633ae3109399642845ae3e33fe957821f09c4
Valeria Titova <valeria.titova.chel@gmail.com>
2023-08-26 11:26:21 +0500

    init, field graphics, tiles prefabs

---
+++ assets/scripts/BlastCore.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('BlastCore')
+export class BlastCore extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
---
+++ assets/scripts/Score.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Score')
+export class Score extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
